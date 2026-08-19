351fc0966cb646b14007298f9217654a343d18ec
fery dwi ramadhi <ferydwir27@gmail.com>
2026-08-19 21:08:16 +0700

    Remove unused documentation and temporary script files

--- test3.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('Frontend/app/guru/tugas/page.tsx', 'utf8');
-const match = c.match(/<select[\s\S]*?className=(["'])([\s\S]*?)\1/gi);
-console.log(match);
